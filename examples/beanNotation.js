9a5db3f32cc68f528b6ff5944eb9a1b3c886679d
Jeff Sanches <jsanchesleao@gmail.com>
2014-02-22 12:27:26 -0300

    cleaning up

--- examples/beanNotation.js
+++
@@ -1,9 +0,0 @@
-var app = require('../');
-
-exports.name = 'test';
-exports.dependencies = app.RESOLVE;
-exports.scope = app.SINGLETON;
-
-exports.factory = function(){
-    return {value: 'test'}
-}
